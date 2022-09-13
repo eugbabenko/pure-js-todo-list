107eb6668f412288bff7dda46c15546f7b513259
Eugene Babenko <eug.babenko@gmail.com>
2022-09-13 17:31:11 +0300

    Fix delete and put http methods

--- back-end/public/main.js
+++ back-end/public/main.js
@@ -1,7 +1,6 @@
 const getTodoList = () => {
-  return fetch("http://localhost:3001/todos")
-  .then((r) => r.json());
-}
+  return fetch("http://localhost:3001/todos").then((r) => r.json());
+};
 
 //________________________________________________________
 
@@ -10,10 +9,10 @@ const inputElement = document.querySelector("#input");
 const msgElement = document.querySelector("#msg");
 const postsElement = document.querySelector("#posts");
 
+let posts = [];
 
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("button clicked");
 
   formValidation();
   inputElement.value = "";
@@ -23,95 +22,82 @@ formElement.addEventListener("submit", (e) => {
 const formValidation = () => {
   if (inputElement.value === "") {
     msgElement.innerHTML = "Post cannot be blank";
-    console.log("failure");
   } else {
-    console.log("successs");
     msgElement.innerHTML = "";
     acceptData();
   }
 };
 
-let posts = [];
-
-let acceptData = () => {
+const acceptData = () => {
   return fetch("http://localhost:3001/todos", {
-    method: 'POST',
+    method: "POST",
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({title: inputElement.value})
-})
-.then(() => loadPosts())
+    body: JSON.stringify({ title: inputElement.value }),
+  }).then(() => loadPosts());
 };
 
 const displayPosts = (posts) => {
-  postsElement.innerHTML = '';
+  postsElement.innerHTML = "";
   posts.map((x) => {
     return (postsElement.innerHTML += `
     <li id=${x.id} class="action">
-      <p class="${x.isCompleted === true ? 'task-title--done' : ''}">${x.title}</p>
+      <p class="${x.isCompleted === true ? "task-title--done" : ""}">${
+      x.title
+    }</p>
       <span class="options">
         <img onClick="setDonePost(this)" src="img/check.png" alt="done">
         <img onClick="deletePost(this)" src="img/x.png" alt="delete">
       </span>
     </li>`);
-  })
+  });
 };
-
 
 let deletePost = (e) => {
   const id = e.parentElement.parentElement.id;
   posts = posts.filter((el) => id !== el.id);
-  displayPosts(posts);
+  deleteData(id);
 };
-
 
 let setDonePost = (e) => {
   const id = e.parentElement.parentElement.id;
   const post = posts.find((el) => el.id === id);
   post.isCompleted = !post.isCompleted;
-  displayPosts(posts);
+  changeData(id, post);
 };
 
-
-
 const sendHttpRequest = (url, method = "GET", body) => {
-  return fetch(url, {method, body})
-    .then(response => {
-      return response.json();
-    });
+  return fetch(url, { method, body }).then((response) => {
+    return response.json();
+  });
 };
 
 const loadPosts = () => {
-  sendHttpRequest('http://localhost:3001/todos/', 'GET')
-    .then(responseData => {
+  sendHttpRequest("http://localhost:3001/todos/", "GET").then(
+    (responseData) => {
       posts = responseData;
       displayPosts(responseData);
-    })
-}
+    }
+  );
+};
+
+const deleteData = (id) => {
+  sendHttpRequest(`http://localhost:3001/todos/${id}`, "DELETE").then(() =>
+    displayPosts(posts)
+  );
+};
+
+const changeData = (id, post) => {
+  fetch(`http://localhost:3001/todos/${id}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  }).then(() => displayPosts(posts));
+};
 
 loadPosts();
-
-// const sendData = () => {
-//   sendHttpRequest('http://localhost:3001/todos/', 'POST')
-//     .then(responseData => {
-//       posts = responseData;
-//       displayPosts(responseData);
-//     })
-// }
-
-// const sendData = () => {
-//   return fetch("http://localhost:3001/todos", {
-//       method: 'POST',
-//       headers: {
-//           'Accept': 'application/json',
-//           'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(Object.assign({}, posts))
-//   })
-//   .then((r) => r.json());
-// }
-
-
-
